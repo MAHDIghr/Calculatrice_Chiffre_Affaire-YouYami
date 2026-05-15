17f634ae7ac194db08f9facb59a4101e1016f97a
Kamel <kamelmahdigherbi@gmail.com>
2026-05-15 20:28:43 +0200

    serving Media Files

--- CA_Calculator/urls.py
+++ CA_Calculator/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('', include('calculator.urls')),
 ]
 
-# Servir les fichiers médias en développement
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Servir les fichiers médias 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
